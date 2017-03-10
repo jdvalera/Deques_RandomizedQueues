6e91ba558a597f2859a43236c85a29653d287285
jdvalera <johnlemuelvalera@yahoo.com>
2017-03-10 15:56:58 -0800

    Added all methods in the API

--- src/Deque.java
+++ src/Deque.java
@@ -1,16 +1,52 @@
 import java.util.Iterator;
 
 public class Deque<Item> implements Iterable<Item> {
-
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	
+	// construct an empty deque
+	public Deque() {
+		// TODO Fill in constructor
 	}
-
+	
+	// is the deque empty?
+	public boolean isEmpty() {
+		return false;	
+	}
+	
+	// return the number of items on the deque
+	public int size() {
+		return 0;
+	}
+	
+	// add the item to the front
+	public void addFirst(Item item) {
+		
+	}
+	
+	// add the item to the end
+	public void addLast(Item item) {
+		
+	}
+	
+	// remove and return the item from the front
+	public Item removeFirst() {
+		return null;
+	}
+	
+	// remove and return the item from the end
+	public Item removeLast() {
+		return null;
+	}
+	
+	// return an iterator over items in order from front to end
 	@Override
 	public Iterator<Item> iterator() {
 		// TODO Auto-generated method stub
 		return null;
+	}
+	
+	// unit testing
+	public static void main(String[] args) {
+
 	}
 
 }
--- src/RandomizedQueue.java
+++ src/RandomizedQueue.java
@@ -1,16 +1,46 @@
 import java.util.Iterator;
 
 public class RandomizedQueue<Item> implements Iterable<Item> {
-
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	
+	// construct an empty randomized queue
+	public RandomizedQueue() {
+		
 	}
-
+	
+	// is the queue empty?
+	public boolean isEmpty() {
+		return false;
+	}
+	
+	// return the number of items on the queue
+	public int size(){
+		return 0;
+	}
+	
+	// add the item
+	public void enqueue(Item item) {
+	}
+	
+	// remove and return a random item
+	public Item dequeue() {
+		return null;
+	}
+	
+	// return (but do not remove) a random item
+	public Item sample() {
+		return null;
+	}
+	
+	// return an independent iterator over items in random order
 	@Override
 	public Iterator<Item> iterator() {
 		// TODO Auto-generated method stub
 		return null;
+	}
+	
+	// unit testing
+	public static void main(String[] args) {
+
 	}
 
 }
