1a8d95acc5cecc22118c2107e2ed4544a2429954
jdvalera <johnlemuelvalera@yahoo.com>
2017-03-10 15:16:08 -0800

    Initial commit

---
+++ src/Deque.java
@@ -0,0 +1,16 @@
+import java.util.Iterator;
+
+public class Deque<Item> implements Iterable<Item> {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public Iterator<Item> iterator() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
