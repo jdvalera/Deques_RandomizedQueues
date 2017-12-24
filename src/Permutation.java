f7823e93062b6fa6d62f846da7b299dbfac7ba4f
jdvalera <johnlemuelvalera@yahoo.com>
2017-12-24 03:14:34 -0800

    Cleaned up code

--- src/Permutation.java
+++ src/Permutation.java
@@ -1,18 +1,24 @@
-import edu.princeton.cs.algs4.In;
+import edu.princeton.cs.algs4.StdIn;
+import edu.princeton.cs.algs4.StdRandom;
 
 public class Permutation {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		int k = Integer.parseInt(args[0]);
-		In in = new In(args[1]);
-		
-		RandomizedQueue rq = new RandomizedQueue<String>();
-		while(!in.isEmpty()) {
+		//In in = new In(args[1]);
+
+		RandomizedQueue<String> rq = new RandomizedQueue<String>();
+		/*while(!in.isEmpty()) {
 			rq.enqueue(in.readString());
+		}*/
+		//for(int i = 0; i < k; i++)
+			//rq.enqueue(StdIn.readString());
+		while(!StdIn.isEmpty()) {
+			rq.enqueue(StdIn.readString());
 		}
 		
-		for(int i = 0; i < k; i++) {
+		for (int i = 0; i < k; i++) {
 			System.out.println(rq.dequeue());
 		}
 	}
--- src/RandomizedQueue.java
+++ src/RandomizedQueue.java
@@ -8,6 +8,7 @@ public class RandomizedQueue<Item> implements Iterable<Item> {
 	private int N = 0;
 	
 	// construct an empty randomized queue
+	@SuppressWarnings("unchecked")
 	public RandomizedQueue() {
 		q = (Item[]) new Object[1];
 		q[0] = null;
@@ -62,6 +63,7 @@ public class RandomizedQueue<Item> implements Iterable<Item> {
 	private class rmIterator implements Iterator<Item> {
 		
 		private int i = N;
+		@SuppressWarnings("unchecked")
 		Item[] copy = (Item[]) new Object[N];
 		
 		public rmIterator() {
@@ -91,6 +93,7 @@ public class RandomizedQueue<Item> implements Iterable<Item> {
 	}
 	
 	private void resize(int capacity) {
+		@SuppressWarnings("unchecked")
 		Item[] copy = (Item[]) new Object[capacity];
 		for (int i = 0; i < N; i++)
 			copy[i] = q[i];
